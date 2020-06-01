26c12704ee08ea6c52376b54f8b76d3f461b6612
Barbara Carvalho <barbara.carvalho@convenia.com.br>
2020-06-01 19:41:07 -0300

    server created

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+	console.log('Listagem de usuários')
+
+	response.json([
+		'Barbara',
+		'Matheus',
+		'Robson',
+		'Juliana',
+		'Diego'
+	]);
+});
+
+app.listen(3333);
